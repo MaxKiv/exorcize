e5305af17b019e7bccbb2fcc67c69c6e609d4b46
MaxKiv <maxkivits42@gmail.com>
2024-07-04 15:46:08 +0200

    feat(haskell): sublist

--- haskell/sublist/src/Sublist.hs
+++ haskell/sublist/src/Sublist.hs
@@ -1,4 +1,10 @@
 module Sublist (sublist) where
 
-sublist :: [a] -> [a] -> Maybe Ordering
-sublist xs ys = error "You need to implement this function."
+import Data.List (isInfixOf)
+
+sublist :: (Eq a) => [a] -> [a] -> Maybe Ordering
+sublist xs ys
+  | xs == ys = Just EQ
+  | xs `isInfixOf` ys = Just LT
+  | ys `isInfixOf` xs = Just GT
+  | otherwise = Nothing
